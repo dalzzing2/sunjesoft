4777305b0e9e05976ec0f9ec8f3d59f527d00801
dalzzing2 <dalzzing2@naver.com>
2018-09-18 16:22:13 +0900

    View For PSA

--- Query/tech_session_psa.sql
+++ Query/tech_session_psa.sql
@@ -1,3 +1,23 @@
+--######################################################################################
+-- View For PSA
+--
+-- MEMBER_NAME      : Member Name
+-- ID             : Session Identifier
+-- SERIAL         : Session Serial
+-- PROGRAM        : Program Name
+-- TOTAL_PSA_MEGA : Maximum Available MegaByte Size
+-- USED_PSA_MEGA  : Allocated MegaByte Size
+-- USED_PSA_PERC  : Percentage Of Allocated Size
+--
+--gSQL> SELECT * FROM TECH_SESSION_PSA;
+--
+--MEMBER_NAME ID SERIAL PROGRAM TOTAL_PSA_MEGA USED_PSA_MEGA USED_PSA_PERC
+------------- -- ------ ------- -------------- ------------- -------------
+--STANDALONE  44      7 gsql    100.00         70.00         70.00              
+--STANDALONE  45     31 gsql    100.00         10.00         10.00              
+--STANDALONE  46     10 gsql    100.00         10.00         10.00              
+--######################################################################################
+
 DROP VIEW IF EXISTS PERFORMANCE_VIEW_SCHEMA.TECH_SESSION_PSA;
 
 CREATE VIEW PERFORMANCE_VIEW_SCHEMA.TECH_SESSION_PSA
@@ -8,7 +28,7 @@ CREATE VIEW PERFORMANCE_VIEW_SCHEMA.TECH_SESSION_PSA
   PROGRAM,
   TOTAL_PSA_MEGA,
   USED_PSA_MEGA,
-  USED_PSA_PERCENTAGE
+  USED_PSA_PERC
 )
 AS
 SELECT
@@ -18,7 +38,7 @@ SELECT
   XS.PROGRAM PROGRAM,
   TO_CHAR(ROUND(XP.VALUE/1024/1024, 2), 'FM99999990.00') TOTAL_PSA_MEGA,
   TO_CHAR(ROUND(SUM(XKPS.VALUE)/1024/1024, 2), 'FM99999990.00') ALLOC_PSA_MEGA,
-  TO_CHAR(ROUND(SUM(XKPS.VALUE)/XP.VALUE*100, 2), 'FM9999990.00') ALLOC_PERCENTAGE  
+  TO_CHAR(ROUND(SUM(XKPS.VALUE)/XP.VALUE*100, 2), 'FM9999990.00') ALLOC_PERCENT
 FROM
   X$KN_PROC_STAT XKPS,
   X$KN_PROC_ENV XKPE,
