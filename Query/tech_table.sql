b119052e9561001db733065457b0a5995bdc0de8
dalzzing2 <dalzzing2@naver.com>
2018-09-18 16:19:06 +0900

    View For Table

--- Query/tech_table.sql
+++ Query/tech_table.sql
@@ -1,3 +1,21 @@
+--######################################################################################
+-- View For Table
+--
+-- OWNER            : Owner
+-- TAB_SCHEMA       : Schema Name
+-- TAB_NAME         : Table Name
+-- LOC_USE_MBYTE    : MegaByte Used By Table In Local
+-- TBS_NAME         : TableSpace Name stored Table
+-- LOC_TBS_USE_PERC : Tablespace Percentage Used By Table
+-- CLU_USE_MBYTE    : MegaByte Used By Table In Cluster
+--
+--gSQL> SELECT * FROM TECH_TABLE;
+--
+--OWNER  TAB_SCHEMA TAB_NAME            LOC_USE_MBYTE TBS_NAME     LOC_TBS_USE_PERC CLU_USE_MBYTE
+-------- ---------- ------------------- ------------- ------------ ---------------- -------------
+--TEST   PUBLIC     TEST_02                      0.50 MEM_DATA_TBS             0.38          4.50
+--######################################################################################
+
 DROP VIEW IF EXISTS PERFORMANCE_VIEW_SCHEMA.TECH_TABLE;
 
 CREATE VIEW PERFORMANCE_VIEW_SCHEMA.TECH_TABLE
